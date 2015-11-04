6d72d1ef9bd11e2c0b0c29078e4152510ace3b88
Student <student@Mac-mini-31.local>
2015-11-04 11:34:49 -0700

    changed url for callback

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "http://www.takemycurb.com/auth/google/callback"
 }, function(accesstoken, refreshToken, profile, done) {
   console.log(profile);
   User.findOne({
